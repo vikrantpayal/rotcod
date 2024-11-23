869546901ca3c9b4460ff40cc5e5f2f92d4c132a
Vikrant Payal <vikrantpayal@gmail.com>
2024-11-23 17:51:30 +0000

    Attempted header panel with dynamic page name

--- main.go
+++ main.go
@@ -1,18 +1,42 @@
 package main
 
 import (
-	"log"
+	"html/template"
 	"net/http"
 )
 
-func main() {
-	// Serve the static files like CSS, JS, and the HTML file
-	fs := http.FileServer(http.Dir("."))
-	http.Handle("/", fs)
+// Struct to pass data to templates
+type PageData struct {
+	PageName string
+}
 
-	// Start the server
-	log.Println("Starting server on http://localhost:8080")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatal(err)
-	}
+func renderPage(w http.ResponseWriter, r *http.Request, pageTitle, templateFile string) {
+	// Prepare data with the dynamic page title
+	data := PageData{PageName: pageTitle}
+
+	// Parse the HTML template
+	tmpl := template.Must(template.ParseFiles(templateFile, "header.html"))
+	tmpl.Execute(w, data)
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	renderPage(w, r, "Home", "home.html")
+}
+
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	renderPage(w, r, "About Us", "about.html")
+}
+
+func main() {
+	// Serve static files (CSS, images, JS, etc.)
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	// Handlers for pages
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/about", aboutHandler)
+
+	// Launch the server
+	println("Server running on http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
 }
